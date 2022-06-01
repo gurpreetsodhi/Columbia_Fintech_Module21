2b945b84d90e9227f42b1f804dca17b2da306f30
Gurpreet Sodhi <gurpreetsodhi@aol.com>
2022-06-01 01:23:46 -0400

    Updated comments

--- KaseiCoin.sol
+++ KaseiCoin.sol
@@ -9,3 +9,20 @@ import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/token/ERC20/ERC20Mintable.sol";
 
 // Create a constructor for the KaseiCoin contract and have the contract inherit the libraries that you imported from OpenZeppelin.
+
+contract KaseiCoin is ERC20, ERC20Detailed, ERC20Mintable {
+
+    address payable owner;
+
+    modifier onlyOwner {
+        require(msg.sender == owner,"You do not have permission to mint these tokens!");
+        _;
+    }
+
+    constructor(string memory name, string memory symbol, uint initial_supply) 
+    ERC20Detailed(name, symbol, 18) 
+    public {
+       // mint(msg.sender, initial_supply);
+    }
+
+}
